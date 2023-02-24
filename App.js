dcb2bf2905fb2f875726c4ef0d6027701a6fe2cc
LayMui <mui_ume@hotmail.com>
2023-02-24 14:54:47 +0800

    Update on the latest version

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,  Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+ 
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
       <StatusBar style="auto" />
     </View>
   );
